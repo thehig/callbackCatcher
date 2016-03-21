431cedd6de63c4800a106eb012180143f9b53adb
fguardiola <fernando.guardiola.888@gmail.com>
2016-03-21 16:40:38 +0000

    Display "hit" time property
    * Add moment js package
    * Add helper to hit template to expose time property in a friendly way
    (using moment js)
    * Display "hits" time property in hits.html

--- callback-catcher/client/templates/hit/hit.js
+++ callback-catcher/client/templates/hit/hit.js
@@ -1,13 +1,27 @@
 Template.hits.helpers({
-	hits: function(){
-		return  Hits.find({}, {sort: {time: -1},limit: 10});
+	hits: function() {
+		return Hits.find({}, {
+			sort: {
+				time: -1
+			},
+			limit: 10
+		});
 	}
 });
 
 Template.entry.helpers({
 	// this: Single HTTP Entry from DB
 	// 	action, headers, body, url
-	headers: function(){ return JSON.stringify(this.headers);},
-	body: function(){ return JSON.stringify(this.body);},
-	action: function(){ return JSON.stringify(this.action.toUpperCase());},
+	headers: function() {
+		return JSON.stringify(this.headers);
+	},
+	body: function() {
+		return JSON.stringify(this.body);
+	},
+	action: function() {
+		return JSON.stringify(this.action.toUpperCase());
+	},
+	time: function() {
+		return moment(this.time).fromNow();
+	}
 });
