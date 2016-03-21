4344cec8444fc0485e9fe07379361cd956b1dbb6
David Higgins <thehig@gmail.com>
2016-03-21 12:39:36 +0000

    Catch all routes

--- callback-catcher/lib/router.js
+++ callback-catcher/lib/router.js
@@ -2,3 +2,14 @@ Router.route('/', function(){
 	console.log("Render /");
 	this.render('Home');
 });
+
+Router.route('/(.*)', {
+  action: function(request) {
+
+	if(!request) Router.go('/');
+
+  	var url = request.url;
+  	console.log("Redirecting " + url + " to /");
+    Router.go('/');
+  }
+});
