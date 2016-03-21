82908afefaae7a2428dddd5dce82325d02f9bfec
David Higgins <thehig@gmail.com>
2016-03-21 13:30:39 +0000

    Add CORS * and autoformat

--- callback-catcher/lib/router.js
+++ callback-catcher/lib/router.js
@@ -1,69 +1,72 @@
 w = console.log;
 
 
-Router.route('/', function(){
+Router.route('/', function() {
 	console.log("Render /");
 	this.render('Home');
 });
 
-if(Meteor.isServer){
+if (Meteor.isServer) {
 
-Router.route('/hits/(.*)', { where: "server" } )
-  .get( function() {
-  	w("get");
+	Router.route('/hits/(.*)', {
+			where: "server"
+		})
+		.get(function() {
+			w("get");
 
-  	Hits.insert({
-  		"action": 'GET',
-  		"url": this.request.url,
-  		"headers": this.request.headers,
-  		"body": this.request.body,
-  	});
+			Hits.insert({
+				"action": 'GET',
+				"url": this.request.url,
+				"headers": this.request.headers,
+				"body": this.request.body,
+			});
 
-  	this.response.statusCode = 200;
-  	this.response.end();
-  })
-  .post( function() {
-  	w("post");
+			this.response.setHeader('access-control-allow-origin', '*');
+			this.response.statusCode = 200;
+			this.response.end();
+		})
+		.post(function() {
+			w("post");
 
 
-  	Hits.insert({
-  		"action": 'POST',
-  		"url": this.request.url,
-  		"headers": this.request.headers,
-  		"body": this.request.body,
-  	});
+			Hits.insert({
+				"action": 'POST',
+				"url": this.request.url,
+				"headers": this.request.headers,
+				"body": this.request.body,
+			});
+			this.response.setHeader('access-control-allow-origin', '*');
+			this.response.statusCode = 200;
+			this.response.end();
+			// If a POST request is made, create the user's profile.
+		})
+		.put(function() {
+			w("put");
 
-  	this.response.statusCode = 200;
-  	this.response.end();
-    // If a POST request is made, create the user's profile.
-  })
-  .put( function() {
-  	w("put");
+			Hits.insert({
+				"action": 'PUT',
+				"url": this.request.url,
+				"headers": this.request.headers,
+				"body": this.request.body,
+			});
+			this.response.setHeader('access-control-allow-origin', '*');
+			this.response.statusCode = 200;
+			this.response.end();
+			// If a PUT request is made, either update the user's profile or
+			// create it if it doesn't already exist.
+		})
+		.delete(function() {
+			w("del");
 
-  	Hits.insert({
-  		"action": 'PUT',
-  		"url": this.request.url,
-  		"headers": this.request.headers,
-  		"body": this.request.body,
-  	});
-
-  	this.response.statusCode = 200;
-  	this.response.end();
-    // If a PUT request is made, either update the user's profile or
-   // create it if it doesn't already exist.
-  })
-  .delete( function() {
-  	w("del");
-
-  	Hits.insert({
-  		"action": 'DELETE',
-  		"url": this.request.url,
-  		"headers": this.request.headers,
-  		"body": this.request.body,
-  	});
-
-  	this.response.statusCode = 200;
-  	this.response.end();
-   // If a DELETE request is made, delete the user's profile.
-  });	
+			Hits.insert({
+				"action": 'DELETE',
+				"url": this.request.url,
+				"headers": this.request.headers,
+				"body": this.request.body,
+			});
+			this.response.setHeader('access-control-allow-origin', '*');
+			this.response.statusCode = 200;
+			this.response.end();
+			// If a DELETE request is made, delete the user's profile.
+		});
 }
