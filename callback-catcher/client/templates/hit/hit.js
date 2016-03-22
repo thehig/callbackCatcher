3dd7daf3b896a27d3dcb6d04b09edf1471bb9985
fguardiola <fernando.guardiola.888@gmail.com>
2016-03-22 11:18:33 +0000

    Display objects JSON format and copy to clipboard
    * Add json2html and clipboard packages
    * Display hits body and headers in a better format (JSON)
    * Add copy to clipboard buttons to be able to copy the hit body in one
    click

--- callback-catcher/client/templates/hit/hit.js
+++ callback-catcher/client/templates/hit/hit.js
@@ -13,10 +13,11 @@ Template.entry.helpers({
 	// this: Single HTTP Entry from DB
 	// 	action, headers, body, url
 	headers: function() {
-		return JSON.stringify(this.headers);
+		return this.headers;
 	},
 	body: function() {
-		return JSON.stringify(this.body);
+		// return JSON.stringify(this.body);
+		return this.body;
 	},
 	action: function() {
 		return JSON.stringify(this.action.toUpperCase());
@@ -24,7 +25,28 @@ Template.entry.helpers({
 	time: function() {
 		return moment(this.time).fromNow();
 	},
-	entryClass:function(){
-		return "verb-"+this.action;
+	entryClass: function() {
+		return "verb-" + this.action;
 	}
+});
+
+
+Template.entry.onRendered(function() {
+	//console.log(this);
+	// console.log(JSON.stringify(this.data));
+	var btnName = ".btn-copy-body-" + this.data._id;
+	// console.log(btnName);
+	var clipboard = new Clipboard(btnName);
+	clipboard.on('success', function(e) {
+		console.info('Action:', e.action);
+		// console.info('Text:', e.text);
+		// console.info('Trigger:', e.trigger);
+
+		e.clearSelection();
+	});
+
+	clipboard.on('error', function(e) {
+		console.error('Action:', e.action);
+		console.error('Trigger:', e.trigger);
+	});
 });
