507bd53d06b6b87c38fb252cab9994fc1dba44bb
David Higgins <thehig@gmail.com>
2016-03-25 15:01:17 +0000

    Remove insecure packages
    * Remove autopublish and insecure
    * Add collection pub/sub
    * Allow remove to all users

--- callback-catcher/lib/collections.js
+++ callback-catcher/lib/collections.js
@@ -1,1 +1,20 @@
 Hits = new Mongo.Collection('hits');
+
+// Allow anyone to remove items from collection
+// https://www.discovermeteor.com/blog/allow-deny-a-security-primer/
+Hits.allow({
+	remove: function(){
+		return true;
+	}
+});
+
+// Pubilsh (server) and subscribe (clients) hits collection
+if(Meteor.isServer){
+	Meteor.publish('hits', function(){
+		return Hits.find();
+	});
+}
+
+if(Meteor.isClient){
+	Meteor.subscribe('hits');
+}
